f4f128dea8c6b01bb06f0c9e2114d701f081f47f
indahqurrothul <56025305+indahqurrothul@users.noreply.github.com>
2020-05-06 11:08:32 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cadangan';
+$config['base_url'] = 'http://localhost/Framework_1C_SiAkademik/';
 
 /*
 |--------------------------------------------------------------------------
