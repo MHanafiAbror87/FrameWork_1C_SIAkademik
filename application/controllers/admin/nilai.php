4e7c09884f3063a4fed4db6a7b4e5a0ebbd58b72
indahqurrothul <56025305+indahqurrothul@users.noreply.github.com>
2020-05-17 18:04:05 +0700

    filter kelas dan tahun akademik cetak uts
    Admin

--- application/controllers/admin/nilai.php
+++ application/controllers/admin/nilai.php
@@ -16,29 +16,35 @@ class nilai extends CI_Controller {
 	}
 
 
-	public function cetak_uts($kode_kelas = "")
+	public function cetak_uts($kode_kelas = "" , $id_tahun_ajaran="")
 	{
 		$d['judul'] = "Data Siswa";
 		if (!empty($kode_kelas)) {
-			$d['nilai_uts'] = $this->Nilai_model->cetak_uts($kode_kelas);
+			$d['nilai_uts'] = $this->Nilai_model->cetak_uts($kode_kelas,$id_tahun_ajaran);
 			$d['kode_kelas'] = $kode_kelas;
+			$d['id_tahun_ajaran'] = $id_tahun_ajaran;
 		} else {
 			$d['nilai_uts'] = "";
 			$d['kode_kelas'] = "";
+			$d['id_tahun_ajaran'] = "";
 		}
 		$d['combo_kelas'] = $this->Combo_model->combo_kelas($kode_kelas);
+		$d['combo_tahun_ajaran'] = $this->Combo_model->combo_tahun_ajaran($id_tahun_ajaran);
 		$this->load->view('admin/top',$d);
 		$this->load->view('admin/menu');
 		$this->load->view('laporan_nilai/cetak_uts');
 		$this->load->view('admin/bottom');	
 	}
 
+
+
 	public function tampil_siswa()
 	{
 		$kode_kelas = $this->input->post("kode_kelas");
-		redirect("admin/nilai/cetak_uts/" . $kode_kelas);
+		$id_tahun_ajaran = $this->input->post("id_tahun_ajaran");
+		redirect("admin/nilai/cetak_uts/" . $kode_kelas ."/".$id_tahun_ajaran);
 	}
-
+	
    
     
 }
